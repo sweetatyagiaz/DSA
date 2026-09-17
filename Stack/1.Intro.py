c1d4081b932890947d5e911d581630bccb0aab27
Rakesh Ranjan <sweetatyagiaz@gmail.com>
2026-09-17 22:05:17 +0530

    Stack

--- Stack/1.Intro.py
+++ Stack/1.Intro.py
@@ -43,4 +43,10 @@ def solve(*args):
 
 if __name__ == "__main__":
     # Example usage / test cases
-    pass
+    stack = create_stack()
+    push(stack, str(1))
+    push(stack, str(2))
+    push(stack, str(3))
+    push(stack, str(4))
+    print("popped item: " + pop(stack))
+    print("stack after popping an element: " + str(stack))
